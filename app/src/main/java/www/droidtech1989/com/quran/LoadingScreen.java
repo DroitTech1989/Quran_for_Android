c7092d7fcf55997d7f4dfe27fcd8f4eedbcd6415
DroidTech <droid.tech.1989@gmail.com>
2018-10-29 21:49:05 +0100

    Initial commit

---
+++ app/src/main/java/www/droidtech1989/com/quran/LoadingScreen.java
@@ -0,0 +1,13 @@
+package www.droidtech1989.com.quran;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+    }
+}
